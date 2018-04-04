0a2ed46872d19bbe5f68702c3b0d9158f9455487
D7703_22 <d7703_22@D7703-22ui-iMac-2.local>
2018-04-04 15:38:00 +0900

    n

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -1,45 +1,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var counter = 1
     var myTimer = Timer()
+    var check = true
+    var checked = true
     
-    @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var imageCounter: UILabel!
-    
+    @IBOutlet weak var myImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageCounter.text = String(counter)
     }
-
+    
     @IBAction func play(_ sender: Any) {
-        // Timer 실행
-       myTimer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
-    }
-    @IBAction func stop(_ sender: Any) {
+        if checked == true {
+            
+            myTimer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+            checked = false
+            
+        } else if checked == false{
+            myTimer.invalidate()
+            checked = true
+        }
         
-        
-        // Timer를 중지
-        myTimer.invalidate()
     }
     
-    // Timer 의해 동적으로 호출되는 함수
+    
+    
     @objc func doAnimation() {
         if counter == 5 {
-            counter = 1
-        } else {
-            counter = counter + 1
+            check = false
+        } else if counter == 1 {
+            check = true
         }
+        if check == true {
+            counter = counter + 1
+        } else {
+            counter = counter - 1
+        }
+        
         myImageView.image = UIImage(named: "frame\(counter).png")
         imageCounter.text = String(counter)
     }
     
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        
     }
-
-
 }
-
